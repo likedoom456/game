563c83b410c3ed784bfb725eced29f1973dd7fa2
likedoom456 <451156713@qq.com>
2016-07-11 18:35:54 +0800

    ...

--- JS/HI.js
+++ JS/HI.js
@@ -5,9 +5,6 @@ var year;
 var month;
 var day;
 
-function birthday() {
-    document.write("告诉我你的生日");
-}
 function input() {
     var name = prompt("输入你的生日");
     year = name[0] + name[1] + name[2] + name[3];
@@ -19,7 +16,7 @@ function timedCount() {
 var d=new Date();
     d.setFullYear(year+80,month,day);
     d.setHours(0,0,0,0);
-  var c=((d.getTime()-new Date().getTime())/1000);
-    document.getElementById("txt").innerHTML=c;
+  var c=((d.getTime()-new Date().getTime())/1000).toFixed(0);
+  document.getElementById("txt").innerHTML="你还有"+c+"秒就挂了哦";
     t=setTimeout('timedCount()','1000');
 }
