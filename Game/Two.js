e540ac868c253cf4def8ebfeaf3e6a5bba04e351
likedoom456 <451156713@qq.com>
2016-07-12 19:29:15 +0800

    game[][]

--- Game/Two.js
+++ Game/Two.js
@@ -1,3 +1,18 @@
 /**
  * Created by Administrator on 2016/7/12.
  */
+var n=0;
+function color(id) {
+    if (n%2==0) {
+        document.getElementById(id).style.backgroundColor='red';
+        n++;
+    }
+    else {
+        color2(id);
+    }
+
+}
+function color2(id) {
+    document.getElementById(id).style.backgroundColor='blue';
+    n++;
+}
